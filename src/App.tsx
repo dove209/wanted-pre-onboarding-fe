a688dcb29f3593f7736cce0fe3039e768ea9bbca
dove209 <53247748+dove209@users.noreply.github.com>
2022-08-14 14:06:09 +0900

    NotFound 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import NotFound from './pages/NotFound';
 import Login from './pages/auth/Login';
 import SignUp from './pages/auth/SignUp';
 import Todos from './pages/todo/Todos';
@@ -14,6 +15,7 @@ function App() {
       <Route path='/signUp' element={<SignUp />} />
       <Route path='/todo/add' element={<TodoAdd />} />
       <Route path='/todo/:id' element={<TodoEdit />} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
