73d9b262cf5acb9fb95e01749fa3ba6b7adc97fe
dove209 <53247748+dove209@users.noreply.github.com>
2022-08-16 18:33:20 +0900

    useMutation에 공통 콜백 함수 제거

--- src/hooks/todo/index.ts
+++ src/hooks/todo/index.ts
@@ -1,8 +1,7 @@
 import { AxiosError } from 'axios';
 import { useMutation, UseMutationResult, useQuery, UseQueryResult } from 'react-query';
 import { getTodosAPI, getTodoByIdAPI, creatTodoAPI, deleteTodoAPI, updateTodoAPI } from '../../api/todo';
-import { ITodo, TodoInputType } from '../../../types/todos';
-
+import { ITodo } from '../../../types/todos';
 
 export const useTodos = (options = {}): UseQueryResult<ITodo[], AxiosError> => {
     return useQuery('todos', getTodosAPI, { ...options });
@@ -16,35 +15,23 @@ export const useTodo = (id: string | undefined, options = {}): UseQueryResult<IT
 }
 
 export const useCreateTodoMuation = (): UseMutationResult<ITodo, AxiosError, any> => {
-    return useMutation(creatTodoAPI, {
-        onSuccess: (date) => {
-            // console.log(date);
-        },
-        onError: (error) => {
-            // console.log(error)
-        }
-    })
+    return useMutation(creatTodoAPI
+    // ,{
+    //     onSuccess: (date) => {
+    //         console.log(date);
+    //     },
+    //     onError: (error) => {
+    //         console.log(error)
+    //     }
+    // }
+    )
 }
 
 export const useUpateTodoMuation = (): UseMutationResult<ITodo, AxiosError, any> => {
-    return useMutation(updateTodoAPI, {
-        onSuccess: (date) => {
-            // console.log(date);
-        },
-        onError: (error) => {
-            // console.log(error)
-        }
-    })
+    return useMutation(updateTodoAPI)
 }
 
 export const useDeleteTodoMuation = (): UseMutationResult<ITodo, AxiosError, any> => {
-    return useMutation(deleteTodoAPI, {
-        onSuccess: (date) => {
-            // console.log(date);
-        },
-        onError: (error) => {
-            // console.log(error)
-        }
-    })
+    return useMutation(deleteTodoAPI)
 }
 
